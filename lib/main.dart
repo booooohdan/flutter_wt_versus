854561ff569af930c6993ecbd22d43258a294e05
Windows\Bohdan <bo7145907@gmail.com>
2022-03-18 05:43:24 +0200

    Added localization

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wt_versus/utilities/introduction_screen_list.dart';
 
 import '../providers/firestore_provider.dart';
 import '../providers/google_signin_provider.dart';
@@ -18,6 +23,8 @@ Future<void> main() async {
   await MobileAds.instance.initialize();
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(statusBarColor: Color(0xFF0000FF)));
+  final prefs = await SharedPreferences.getInstance();
+  final _skipIntroduction = prefs.getBool('skipIntroduction') ?? false;
   runApp(
     MultiProvider(
       providers: [
@@ -41,6 +48,17 @@ Future<void> main() async {
           scaffoldBackgroundColor: Color(0xFFDCDCDC),
         ),
         darkTheme: ThemeData.dark(),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          AppLocalizations.delegate,
+        ],
+        supportedLocales: [
+          const Locale('en', ''),
+          const Locale('ru', ''),
+          const Locale('uk', ''),
+        ],
         debugShowCheckedModeBanner: false,
         home: StreamBuilder(
           stream: FirebaseAuth.instance.authStateChanges(),
@@ -49,14 +67,30 @@ Future<void> main() async {
             if (snapshot.connectionState == ConnectionState.waiting) {
               return Center(child: CircularProgressIndicator());
             } else if (snapshot.hasData) {
-              return BottomNavBar();
+              return _skipIntroduction
+                  ? BottomNavBar()
+                  : IntroductionScreen(
+                      pages: getIntroductionPages(),
+                      showBackButton: true,
+                      back: const Icon(Icons.navigate_before),
+                      next: const Icon(Icons.navigate_next),
+                      done: Text(AppLocalizations.of(context)!.done),
+                      onDone: () {
+                        prefs.setBool('skipIntroduction', true);
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => BottomNavBar()),
+                        );
+                      },
+                    );
+              // Main entry point
             } else if (snapshot.hasError) {
               return Center(child: Text('Something Went Wrong!'));
             } else {
               return SignUpScreen();
             }
           },
-        ), //BottomNavBar(),
+        ),
       ),
     ),
   );
--- lib/screens/select_screen.dart
+++ lib/screens/select_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:introduction_screen/introduction_screen.dart';
 import 'package:provider/provider.dart';
-
+import 'package:wt_versus/screens/plane_comparison_screen.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import '../models/plane.dart';
 import '../providers/firestore_provider.dart';
-import '../utilities/introduction_screen_list.dart';
-import 'plane_comparison_screen.dart';
 
 class SelectScreen extends StatefulWidget {
   const SelectScreen({Key? key}) : super(key: key);
@@ -17,7 +16,6 @@ class SelectScreen extends StatefulWidget {
 }
 
 class _SelectScreenState extends State<SelectScreen> {
-  bool _isFirstLaunch = true; //TODO: Change to shared preference (default is true)
   bool _isFirstInit = false;
   int? _groupValue = 0;
 
@@ -72,180 +70,176 @@ class _SelectScreenState extends State<SelectScreen> {
     final screenSize = MediaQuery.of(context).size;
     final appbarSize = AppBar().preferredSize.height;
     final theme = Theme.of(context);
+    final localizations = AppLocalizations.of(context)!;
     return SafeArea(
       child: Scaffold(
-        body: _isFirstLaunch
-            ? IntroductionScreen(
-                pages: getIntroductionPages(),
-                showBackButton: true,
-                back: const Icon(Icons.navigate_before),
-                next: const Icon(Icons.navigate_next),
-                done: Text('Done'),
-                onDone: () {
-                  _isFirstLaunch = false; //TODO: Change to shared preference
-                },
-              )
-            : CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    expandedHeight: appbarSize * 3,
-                    floating: true,
-                    snap: true,
-                    pinned: true,
-                    elevation: 0,
-                    flexibleSpace: FlexibleSpaceBar(
-                      background: Container(
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                              image: NetworkImage('https://cdn.pixabay.com/photo/2017/06/09/10/40/colour-2386473_960_720.jpg'), fit: BoxFit.fill),
-                        ),
-                        child: Column(
-                          children: [
-                            SizedBox(
-                              height: 20,
-                            ),
-                            CupertinoSlidingSegmentedControl<int>(
-                              backgroundColor: CupertinoColors.white,
-                              thumbColor: theme.colorScheme.primary,
-                              groupValue: _groupValue,
-                              children: {
-                                0: Container(
-                                  child: Text(
-                                    'Aviation',
-                                    style: TextStyle(fontSize: 18, color: Colors.black),
-                                  ),
-                                ),
-                                1: Container(
-                                  child: Text(
-                                    'Army',
-                                    style: TextStyle(fontSize: 18, color: Colors.black),
-                                  ),
-                                ),
-                                2: Container(
-                                  child: Text(
-                                    'Ships',
-                                    style: TextStyle(fontSize: 18, color: Colors.black),
-                                  ),
-                                ),
-                              },
-                              onValueChanged: (value) {
-                                setState(() {
-                                  _groupValue = value;
-                                });
-                              },
-                            ),
-                            SizedBox(
-                              height: 20,
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  'Planes: ',
-                                  style: TextStyle(color: Colors.white, fontSize: 30),
-                                ),
-                                Text(
-                                  _searchResult.length.toString(),
-                                  style: TextStyle(color: Colors.white, fontSize: 30),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              expandedHeight: appbarSize * 3,
+              floating: true,
+              snap: true,
+              pinned: true,
+              elevation: 0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage('https://cdn.pixabay.com/photo/2017/06/09/10/40/colour-2386473_960_720.jpg'), fit: BoxFit.fill),
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 20,
                       ),
-                    ),
-                    bottom: PreferredSize(
-                      preferredSize: Size.fromHeight(appbarSize),
-                      child: Row(
+                      CupertinoSlidingSegmentedControl<int>(
+                        backgroundColor: CupertinoColors.white,
+                        thumbColor: theme.colorScheme.primary,
+                        groupValue: _groupValue,
+                        children: {
+                          0: Container(
+                            child: Text(
+                              localizations.planes,
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                          1: Container(
+                            child: Text(
+                              localizations.army,
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                          2: Container(
+                            child: Text(
+                              localizations.heli,
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                          3: Container(
+                            child: Text(
+                              localizations.fleet,
+                              style: TextStyle(fontSize: 18, color: Colors.black),
+                            ),
+                          ),
+                        },
+                        onValueChanged: (value) {
+                          setState(() {
+                            _groupValue = value;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              //TODO: Add filters
-                            },
-                            child: Text(
-                              'Filter',
-                              style: theme.textTheme.button,
-                            ),
+                          Text(
+                            'Planes: ',
+                            style: TextStyle(color: Colors.white, fontSize: 30),
                           ),
-                          SizedBox(width: 20),
-                          Expanded(
-                            child: TextField(
-                              controller: _searchController,
-                              onChanged: (value) {},
-                              decoration: const InputDecoration(
-                                labelText: 'Search',
-                                labelStyle: TextStyle(color: Colors.white),
-                                suffixIcon: Icon(Icons.search),
-                                suffixIconColor: Colors.white,
-                              ),
-                              style: TextStyle(fontSize: 14),
-                            ),
+                          Text(
+                            _searchResult.length.toString(),
+                            style: TextStyle(color: Colors.white, fontSize: 30),
                           ),
-                          SizedBox(width: 20),
-                          ElevatedButton(
-                            onPressed: () {
-                              _selectedVehicles.clear();
-                              setState(() {});
-                            },
-                            child: Text(
-                              'Clear',
-                              style: theme.textTheme.button,
-                            ),
-                          ),
-                          SizedBox(width: 20),
                         ],
                       ),
-                    ),
+                    ],
                   ),
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (context, index) {
-                        final _vehicleSelected = _selectedVehicles.contains(_searchResult[index]);
-                        return Padding(
-                          padding: const EdgeInsets.all(8),
-                          child: ListTile(
-                            leading: Container(
-                              height: 50,
-                              child: SvgPicture.asset(
-                                'assets/icons/${_searchResult[index].nation}.svg',
-                                height: screenSize.height / 20,
-                              ),
-                            ),
-                            title: Text('[${_searchResult[index].BRs[1]}] ${_searchResult[index].name}'),
-                            subtitle: Text(_searchResult[index].nation),
-                            trailing: _vehicleSelected ? Icon(Icons.check) : Icon(Icons.check, size: 0),
-                            tileColor: Colors.white,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-                            onTap: () {
-                              setState(() {
-                                if (_vehicleSelected) {
-                                  _selectedVehicles.remove(_searchResult[index]);
-                                } else {
-                                  _selectedVehicles.add(_searchResult[index]);
-                                }
-                              });
-                            },
-                            selected: _vehicleSelected,
-                          ),
-                        );
-                      },
-                      childCount: _searchResult.length,
-                    ),
-                  )
-                ],
+                ),
               ),
-        // floatingActionButton: FloatingActionButton.extended(
-        //   elevation: 0,
-        //   icon: Icon(Icons.image),
-        //   label: Text('Compare'),
-        //   onPressed: () {
-        //     Navigator.push(
-        //       context,
-        //       MaterialPageRoute(builder: (context) => PlaneComparisonScreen(receivedData: _selectedVehicles)),
-        //     );
-        //   },
-        // ),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(appbarSize),
+                child: Row(
+                  children: [
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        //TODO: Add filters
+                      },
+                      child: Text(
+                        'Filter',
+                        style: theme.textTheme.button,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    Expanded(
+                      child: TextField(
+                        controller: _searchController,
+                        onChanged: (value) {},
+                        decoration: const InputDecoration(
+                          labelText: 'Search',
+                          labelStyle: TextStyle(color: Colors.white),
+                          suffixIcon: Icon(Icons.search),
+                          suffixIconColor: Colors.white,
+                        ),
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        _selectedVehicles.clear();
+                        setState(() {});
+                      },
+                      child: Text(
+                        'Clear',
+                        style: theme.textTheme.button,
+                      ),
+                    ),
+                    SizedBox(width: 20),
+                  ],
+                ),
+              ),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
+                (context, index) {
+                  final _vehicleSelected = _selectedVehicles.contains(_searchResult[index]);
+                  return Padding(
+                    padding: const EdgeInsets.all(8),
+                    child: ListTile(
+                      leading: Container(
+                        height: 50,
+                        child: SvgPicture.asset(
+                          'assets/icons/${_searchResult[index].nation}.svg',
+                          height: screenSize.height / 20,
+                        ),
+                      ),
+                      title: Text('[${_searchResult[index].BRs[1]}] ${_searchResult[index].name}'),
+                      subtitle: Text(_searchResult[index].nation),
+                      trailing: _vehicleSelected ? Icon(Icons.check) : Icon(Icons.check, size: 0),
+                      tileColor: Colors.white,
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                      onTap: () {
+                        setState(() {
+                          if (_vehicleSelected) {
+                            _selectedVehicles.remove(_searchResult[index]);
+                          } else {
+                            _selectedVehicles.add(_searchResult[index]);
+                          }
+                        });
+                      },
+                      selected: _vehicleSelected,
+                    ),
+                  );
+                },
+                childCount: _searchResult.length,
+              ),
+            )
+          ],
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+          elevation: 0,
+          icon: Icon(Icons.image),
+          label: Text(localizations.compare),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => PlaneComparisonScreen(receivedData: _selectedVehicles)),
+            );
+          },
+        ),
       ),
     );
   }
